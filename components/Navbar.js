2e5934ec1ffbde9f624c8f778c74e523b15d1712
Hanish Jindal <hanish1139.cse19@chitkara.edu.in>
2023-04-09 11:38:49 +0530

    Dev V2.1 mobile navbar button fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -172,7 +172,7 @@ const Navbar = () => {
             <Link href="/login">Log in</Link>
           </button>
           <button
-            className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 w-48"
+            className="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 w-40"
             onClick={toggle}
           >
             <Link href="/dashboard">Dashboard</Link>
